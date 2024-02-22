bca731a9035537e4ca273b5b1fabb89e72b0ce08
shboe <151910459+shboe@users.noreply.github.com>
2024-02-22 11:35:50 +0900

    リヤン前編集してたの内容

---
+++ template/vegan.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import mysql.connector
+
+
+app = Flask(__name__)
+
+# Establish MySQL connection
+db = mysql.connector.connect(
+    host="", #your_mysql_host
+    user="", #your_mysql_user
+    password="", #your_mysql_password
+    database="" #your_mysql_database
+)
+
+cursor = db.cursor()
+
+
+@app.route('/')
+def index():
+    return render_template('homepage.html')
+
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
