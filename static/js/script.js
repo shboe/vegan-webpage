0522ef0bdaff2e16575ee9c6aa354c6542b1a36d
ryanalexanderl <ryanalexanderleo@gmail.com>
2024-02-27 12:25:41 +0900

    完成
    added clear button to remove the checked condition

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,8 @@ function submitForm() {
     document.getElementById("filter").submit();
 }
 
+    uncheckButton.addEventListener('click', () => {
+        radioButtons.forEach(button => {
+            button.checked = false;
+        });
+    });
